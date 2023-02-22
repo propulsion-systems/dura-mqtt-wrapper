1b70a2dfcd3cf60383cc5f97c9ffba6e9d4f8953
Jonah Ruymbeeck <ruymbeeckjonah@gmail.com>
2023-02-22 20:35:41 +0100

    Implemented matches

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -16,10 +16,56 @@ interface Match {
 }
 
 export function matches({ topic, matcher }: Match): boolean {
-  console.log(topic);
-  console.log(matcher);
+  // Matcher: #
+  if (matcher === '#') {
+    return true;
+  }
 
-  return false;
+  // Topic: /foo, matcher: /foo
+  if (topic === matcher) {
+    return true;
+  }
+
+  const topicFragments = topic.split('/')
+  const matcherFragments = matcher.split('/')
+
+  // no match yet so we need to check each fragment
+  for (var i = 0; i < matcherFragments.length; i++) {
+    const lhsFragment = matcherFragments[i];
+    // If we've reached a multi wildcard in the lhs rawTopic,
+    // we have a match.
+    // (this is the mqtt spec rule finance matches finance or finance/#)
+    if (lhsFragment === '#') {
+      return true;
+    }
+
+    const isLhsWildcard = lhsFragment === '+';
+    // If we've reached a wildcard match but the matchee does
+    // not have anything at this fragment level then it's not a match.
+    // (this is the MQTT spec rule 'finance does not match finance/+'
+    if (isLhsWildcard && topicFragments.length <= i) {
+      return false;
+    }
+
+    // if lhs is not a wildcard we need to check whether the
+    // two fragments match each other.
+    if (!isLhsWildcard) {
+      const rhsFragment = topicFragments[i];
+      // If the hs fragment is not wildcard then we need an exact match
+      if (lhsFragment !== rhsFragment) {
+        return false;
+      }
+    }
+    // If we're at the last fragment of the lhs rawTopic but there are
+    // more fragments in the in the matchee then the matchee rawTopic
+    // is too specific to be a match.
+    if (i + 1 == matcherFragments.length && topicFragments.length > matcherFragments.length) {
+      return false;
+    }
+    // If we're here the current fragment matches so check the next
+  }
+
+  return true;
 }
 
 export function onMessage<T>({ topic, payload }: Message, subscriptions: Subscriptions<T>) {
---
+++ test/match.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from 'vitest'
+import { matches } from '../src/mqtt'
+
+const tests = [
+  { topic: 'foo', matcher: 'foo', expectation: true },
+  { topic: 'foo', matcher: '#', expectation: true },
+  { topic: 'foo', matcher: '+', expectation: true },
+  { topic: 'foo/bar', matcher: 'foo/#', expectation: true },
+  { topic: 'foo/bar', matcher: 'foo/+', expectation: true },
+  { topic: 'foo/bar', matcher: 'foo/bar', expectation: true },
+  { topic: 'foo/baz', matcher: 'foo/bar', expectation: false },
+  { topic: 'foo/baz/bizz', matcher: 'foo/+/baz', expectation: false },
+  { topic: 'foo', matcher: 'foo/+/baz', expectation: false },
+]
+
+for (const { topic, matcher, expectation } of tests) {
+  test(`Topic: ${topic}, matcher: ${matcher} -> ${expectation}`, () => {
+    expect(matches({ topic, matcher })).toBe(expectation)
+  })
+}
